fdc68e42142d658211a750a93163fd7fa6803de7
Murtaza Ali <175622130+Murtaza-626@users.noreply.github.com>
2026-01-16 22:37:57 +0500

    Update pdf_analyzer.py

--- pdf_analyzer.py
+++ pdf_analyzer.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 # LangChain imports
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
@@ -182,6 +183,7 @@ with tab2:
 
 st.divider()
 st.info(f"Using Ollama Model: {OLLAMA_MODEL}")
+
 
 
 
